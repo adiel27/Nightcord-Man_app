392983719e980e77bbfd4f96e0f9c164dc194fe5
CATSS <85265087+CATSSRIN@users.noreply.github.com>
2025-06-12 14:41:45 +0700

    hehe

--- keuangan.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE keuangan (
-    id_transaksi INT AUTO_INCREMENT PRIMARY KEY,
-    id_artis INT,
-    jenis_transaksi VARCHAR(100) NOT NULL,
-    jumlah_uang DECIMAL(15,2) NOT NULL,
-    tanggal DATE NOT NULL,
-    deskripsi TEXT,
-    FOREIGN KEY (id_artis) REFERENCES artis(id)
-);
